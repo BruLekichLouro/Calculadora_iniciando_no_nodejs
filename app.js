bc830f67952146f77cd88e420c71faf6a753d091
BruLekichLouro <bububio@yahoo.com.br>
2023-05-09 11:41:12 -0300

    Criando servidor

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var calculadora = require("./operacoes");
+var http = require("http");
 
-function subt(a,b){
-    return a-b;
-}
-function mult(a,b){
-    return a*b;
-}
-function div(a,b){
-    return a/b;
-}
+//Criando servidor: qq outra pessoa poderia acessar
+http.createServer(function(requisicao, resposta){
+    resposta.end("Bem vindo ao meu site");
+}).listen(2727);
+console.log("Meu servidor está rodando!");
+//No navagador eu digito: http://localhost:2727
 
 console.log(calculadora.mult(10,3));
 console.log(calculadora.div(10,5));
+
+
